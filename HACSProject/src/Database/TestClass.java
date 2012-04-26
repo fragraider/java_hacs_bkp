4ca6e8120b4b7f45404f2ce5d06936abfa570fac
Notandi <Notandi@Notandi-PC>
2012-04-27 01:30:23 +0200

    4rth Commit. Im still fighting the code and this is just the database part. Running the Testclass with a getConfig should work. Remember that the SQL script is on Dropbox along with other information.

--- HACSProject/src/Database/TestClass.java
+++ HACSProject/src/Database/TestClass.java
@@ -7,11 +7,33 @@ import java.sql.SQLException;
 
 public class TestClass {
     
+    
+    //Testing for CreateExecStatement
+/* public static void main(String[] args) throws SQLException {
+    CreateExecStatement createExec = new CreateExecStatement();
+    createExec.queryExecution("SELECT * FROM  \"HACSDB\"");
+    }
+    */
+ 
+    //Testing for QueryExchange.newUser;
+/* public static void main(String[] args) throws SQLException {
+    QueryExchange test1 = new QueryExchange();
+    test1.newUser(); 
+    }
+    */
+    
+    //Testing for CreateExecStatement.updateExecution()
 public static void main(String[] args) throws SQLException {
     CreateExecStatement createExec = new CreateExecStatement();
-    createExec.testMethod();
-}
- 
-
-
+    createExec.updateExecution();
+    }
+    
+    
+    
+        //Testing for QueryExchange.getConfig
+/* public static void main(String[] args) throws SQLException {
+    QueryExchange test1 = new QueryExchange();
+    test1.getConfig("SELECT * FROM HACSDB WHERE RFID = ('1AAF23')");
+    * }
+    */
 }
